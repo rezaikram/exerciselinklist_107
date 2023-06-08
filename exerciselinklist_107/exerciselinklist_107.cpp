1a5b3e2898fde44532f8fad46a96f8a8921370f5
rezaikram <muhammadikramriza@gmail.com>
2023-06-08 14:58:41 +0700

    Add project files.

---
+++ exerciselinklist_107/exerciselinklist_107.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Node {
+
+public:
+	int Muhammad;
+	string Ikramriza;
+	Node* next;
+};
+class CircularLinkedList {
+private:
+	Node* LAST;
+public:
+	CircularLinkedList() {
+		LAST = NULL;
+	}
+	void addNode();
+	bool search(int rollno, Node** previous, Node** current);
+	bool listEmpty();
+	bool delNode();
+	void traverse();
+};
+void CircularLinkedList::addNode() { //write your answer here
+}
+bool CircularLinkedList::search(int rollno, Node** previous, Node** current) {
+	*previous = LAST->next;
+	*current = LAST->next;
+	while (*current != LAST) {
+		if (rollno == (*current)->Muhammad) {
+			return true;
+		}
+		*previous = *current;
+		*current = (*current)->next;
+	}
+	if (rollno == LAST->Muhammad) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+bool CircularLinkedList::listEmpty() {
+	return LAST == NULL;
+}
+bool CircularLinkedList::delNode() { //write your answer here
+}
+void CircularLinkedList::traverse() {
+	if (listEmpty()) {
+		cout << "\nList is empty\n";
+	}
+	else {
+		cout << "\nRecords in the list are:\n";
+		Node* currentNode = LAST->next;
+		while (currentNode != LAST) {
+			cout << currentNode->Muhammad << " " << currentNode->Ikramriza << endl;
+			currentNode = currentNode->next;
+		}
+		cout << LAST->Muhammad << " " << LAST->Ikramriza << endl;
+	}
+}
+int main() {
+	CircularLinkedList obj;
+	while (true) {
+		try {
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. View all the records in the list" << endl;
+			cout << "4. Exit" << endl;
+			cout << "\nEnter your choice (1-5): ";
+			char ch;
+			cin >> ch;
+			switch (ch) {
+			case '1': {
+			}
+			case '2': {
+			}
+			case '3': {
+				obj.traverse();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "Invalid option" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << e.what() << endl;
+		}
+	}
+	return 0;
+}
